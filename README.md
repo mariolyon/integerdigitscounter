da622118de596023eb81a1bc6756d8bff96e889e
Mario Lyon <mario@digileo.com>
2026-01-22 08:12:14 +0100

    add Readme and show usage message if run without arguments

---
+++ README.md
@@ -0,0 +1,30 @@
+Integer Digits Counter
+======================
+
+Counts the number of digits in the integer part of a number.
+Negative numbers and all number lengths are supported. 
+
+# Run
+Run the application for specific number using mill:
+
+```sh
+./mill app.run -1234.56
+```
+
+This will print 4.
+
+## Tests
+Run the tests using:
+```sh
+./mill app.test
+```
+
+## Implementation
+Relies on BigDecimal to handle all numeric strings from the console. 
+
+digileo.demo.DigitisCounter.countDigits handles java.lang.Number instances. 
+
+Implemented with Scala 3, Mill, and MUnit
+
+## Author
+Mario Lyon <mario@digileo.com>
--- app/src/App.scala
+++ app/src/App.scala
@@ -4,20 +4,31 @@ import java.lang.NumberFormatException
 
 import DigitsCounter._
 
-@main def run(string: String): Unit = {
+@main def run(args: String*): Unit = {
+  if (args.isEmpty || args.size > 1) {
+    printUsage()
+  } else {
+    val string = args.head
+    val number =
+      try {
+        javaNumberFromString(string)
+      } catch {
+        case _: NumberFormatException =>
+          Console.err.println("Error: given string is an invalid number")
+          sys.exit(-1)
+      }
 
-  val number =
-    try {
-      javaNumberFromString(string)
-    } catch {
-      case _: NumberFormatException =>
-        Console.err.println("Error: given string is an invalid number")
-        sys.exit(-1)
-    }
-
-  val result = countDigits(number)
-  println(result)
+    val result = countDigits(number)
+    println(result)
+  }
 }
+
+def printUsage(): Unit =
+  println("""
+            ||Integer Digits Counter|
+            |Example Usage: ./mill app.run -1234.12
+            |           Or: ./mill app.run 4355
+    """.stripMargin)
 
 def javaNumberFromString(string: String): java.lang.Number =
   new java.math.BigDecimal(string)
