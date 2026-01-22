6bf8612ed42807d016d8f8fb95844fc88f5fe2b5
Mario Lyon <mario@digileo.com>
2026-01-22 15:36:07 +0100

    refactor to idiomatically handle non-numeric strings

--- app/src/App.scala
+++ app/src/App.scala
@@ -4,31 +4,31 @@ import java.lang.NumberFormatException
 
 import DigitsCounter._
 
-@main def run(args: String*): Unit = {
+@main def run(args: String*): Unit =
   if (args.isEmpty || args.size > 1) {
     printUsage()
   } else {
     val string = args.head
-    val number =
-      try {
-        javaNumberFromString(string)
-      } catch {
-        case _: NumberFormatException =>
-          Console.err.println("Error: given string is an invalid number")
-          sys.exit(-1)
-      }
-
-    val result = countDigits(number)
-    println(result)
+    javaNumberFromString(string) match {
+      case Some(number) =>
+        val result = countDigits(number)
+        println(result)
+      case _ =>
+        Console.err.println("Error: given string is an invalid number")
+        sys.exit(-1)
+    }
   }
-}
 
-def printUsage(): Unit =
+private def printUsage(): Unit =
   println("""
             ||Integer Digits Counter|
             |Example Usage: ./mill app.run -1234.12
             |           Or: ./mill app.run 4355
     """.stripMargin)
 
-def javaNumberFromString(string: String): java.lang.Number =
-  new java.math.BigDecimal(string)
+protected def javaNumberFromString(string: String): Option[java.lang.Number] =
+  try {
+    Some(new java.math.BigDecimal(string))
+  } catch {
+    case _: NumberFormatException => None
+  }
--- app/test/src/AppTest.scala
+++ app/test/src/AppTest.scala
@@ -4,17 +4,17 @@ import munit.FunSuite
 import digileo.demo.javaNumberFromString
 
 class AppTest extends FunSuite {
-  test("given numeric strings, then return java.lang.Number instances") {
+  test("given numeric strings, then return Some java.lang.Number") {
     for (s <- Seq("123.456", "19", "0", "-1")) {
-      assert(javaNumberFromString(s).isInstanceOf[java.lang.Number])
+      val actual = javaNumberFromString(s)
+      actual.isDefined
+      assert(actual.get.isInstanceOf[java.lang.Number])
     }
   }
 
-  test("given non numeric strings, then throw NumberFormatException") {
+  test("given non numeric strings, return None") {
     for (s <- Seq("123.456.111", "abc")) {
-      intercept[NumberFormatException] {
-        javaNumberFromString(s)
-      }
+        assert(javaNumberFromString(s).isEmpty)
     }
   }
 }
